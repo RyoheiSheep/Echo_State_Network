9ff833aece2a29072e58137b2f89e2d084922706
RyoheiSheep <48007637+RyoheiSheep@users.noreply.github.com>
2020-01-24 20:17:45 +0900

    output バイアスを実装

--- Echo_State_Network/ESN.py
+++ Echo_State_Network/ESN.py
@@ -7,7 +7,7 @@ from scipy import linalg
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import time
-from Functions import get_reservoir_states, train, one_step_predict, RMSE
+from Functions import get_reservoir_states, train, one_step_predict, RMSE, free_run_predict
 
 
 start_time = time.time()
@@ -25,6 +25,9 @@ if input_bias is None:
     pass
 else:
     num_input_node = num_input_node + 1
+    
+output_bias = None
+
 ##################################################################
 output_return = None
 #############  parameters of training and data####################
@@ -40,7 +43,8 @@ num_time_step = int(T/dt)
 
 wash_time = 100
 length_train = 50000
-length_prediction = 10000
+length_prediction = 9000
+length_free_run = 1000
 # length_freerun_prediction  = 500
     
 RATIO_TRAIN = 0.6
@@ -51,7 +55,7 @@ LEAK_RATE=0.02
 ######################################################################
 ############### preparation of data #################################
 #####################################################################
-teacher_data = np.loadtxt("Lorentz.txt")        ######学習用データ
+teacher_data = np.loadtxt("Rossler.txt")[::10]       ######学習用データ
 # teacher_input_init = np.array([np.loadtxt("logistic.txt")]).T
 teacher_input = teacher_data[wash_time :-1]
 teacher_output = teacher_data[wash_time +1:]
@@ -96,7 +100,9 @@ weight_return_init = matrix_nonzero * weight_return_init
 ##############################################################################################################################
 
 ######################  Get State  ##################################
+
 initial_state = np.zeros((len(teacher_data), num_reservoir_node))
+
 states = get_reservoir_states(inputs = teacher_data, states_init = initial_state, 
                                 weight_input= weight_input_init, weight_reservoir=weight_reservoir_init, 
                                 leak_rate = LEAK_RATE, weight_return = weight_return_init,
@@ -107,11 +113,19 @@ states = get_reservoir_states(inputs = teacher_data, states_init = initial_state
 states = states[wash_time: ]
 #####################  Train  Output Weight   #########################
 
-weight_output =  train(num_reservoir_node = num_reservoir_node, num_output_node = num_output_node, 
-                        train_data_output = train_data_output,
-                        states = states, weight_output_initial = weight_output_init,
-                        LAMBDA = 0.00221)
-
+if output_bias is None:
+    weight_output =  train(num_reservoir_node = num_reservoir_node, num_output_node = num_output_node, 
+                            train_data_output = train_data_output,
+                            states = states, weight_output_initial = weight_output_init,
+                            LAMBDA = 0.00221)
+else:
+    states = np.column_stack((np.ones((states.shape[0],1)),states))
+    weight_output_init = np.zeros((num_reservoir_node+1, num_output_node))
+    weight_output =  train(num_reservoir_node = num_reservoir_node+1, num_output_node = num_output_node, 
+                            train_data_output = train_data_output,
+                            states = states, 
+                            weight_output_initial = weight_output_init,
+                            LAMBDA = 0.00221)
 #**********************************************************************************************************************************************#
 ############################################################################################################################### 
 ####################   Prediction #############################################################################################
@@ -132,6 +146,11 @@ one_step_predict_train = one_step_predict(num_output_node= num_output_node,
 one_step_predict_test = one_step_predict(num_output_node= num_output_node, states=states[length_train:], 
                                         weight_output = weight_output, 
                                         length_prediction= length_prediction)
+############# Free Running Prediction  #####################################
+
+# free_run_prediction_test = free_run_predict(states = states[length_train:], num_output_node = num_output_node, weight_output = weight_output,
+                                            # weight_input = weight_input_init, weight_reservoir = weight_reservoir, 
+                                            # length_freerun = length_free_run, leak_rate = LEAK_RATE, input_bias = input_bias)
 
 print("length of one step predict test")
 print(len(one_step_predict_test))
@@ -179,7 +198,7 @@ ax1.plot(one_step_predict_train.T[0,:], one_step_predict_train.T[1,:], one_step_
 ax1.set_title(" One Step Prediction")
 
 
-################# Comparison of Free Running and One Step  ##############################
+################# Comparison of Real Time Series and One Step Prediction ##############################
 
 # ax.plot(teacher_input_init.T[0,length_train:], teacher_input_init.T[1,length_train:], teacher_input_init.T[2,length_train:], color = "magenta", linewidth = 0.1)
 # ax.set_title("real time series")
@@ -191,6 +210,16 @@ plt.plot(one_step_predict_test.T[0,:100],lw = 1.0,  color = "blue", label ="one 
 plt.plot(test_data_output.T[0][:100],lw = 1.0,  color = "red", label ="real time series")
 plt.title('x of one step prediction and real time series')
 plt.legend()
+
+################### Comparison of Free Running Prediction and Real Time Series  ###################
+
+# plt.figure()
+# plt.plot(free_run_predict_test.T[0,:100], lw = 1.0, color ="red", label ="x of free runnning prediction")
+# # plt.plot(one_step_predict_test.T[0,:100],lw = 1.0,  color = "blue", label ="one step prediction")
+# plt.plot(test_data_output.T[0][:100],lw = 1.0,  color = "red", label ="real time series")
+# plt.title('x of free running prediction and real time series')
+# plt.legend()
+
 
 ###################    Teacher Data     #############################################
 
@@ -204,10 +233,16 @@ plt.legend()
 ############## state[t],   state[t + delta],   state[t + 2delta]########################
 fig2= plt.figure()
 ax2 = Axes3D(fig2)
-ax2.plot(states.T[0,wash_time: 10000], states.T[0,wash_time+15: 10015], states.T[0,wash_time+30: 10030], color = "red", linewidth = 0.1 )
+ax2.plot(states.T[1,wash_time: 10000], states.T[1,wash_time+15: 10015], states.T[1,wash_time+30: 10030], color = "red", linewidth = 0.1 )
 ax2.set_title("states delayed axis")
 
 #######################################################################################
+##################state[1], state [2], state[3]#######################################
+fig2= plt.figure()
+ax2 = Axes3D(fig2)
+ax2.plot(states.T[1,wash_time: 10000], states.T[2,wash_time: 10000], states.T[3,wash_time: 10000], color = "red", linewidth = 0.1 )
+ax2.set_title("states projected into 3dimensions")
+
 
 plt.show()
 #******************************************************************************************************************************#
