4fd052f4c41622db1d240370a65c69243c2ff04b
RyoheiSheep <48007637+RyoheiSheep@users.noreply.github.com>
2020-01-30 01:14:15 +0900

    nolds によるリアプノフ指数の計算

--- Echo_State_Network/Lyapunov.py
+++ Echo_State_Network/Lyapunov.py
@@ -1,9 +1,9 @@
 import nolds
 import numpy as np
 
-data = np.loadtxt("Henon.txt").T[0,:60000]
+data = np.loadtxt("Henon.txt").T[0,:10000]
 
-lyapunov_max = nolds.lyap_r(data = data,emb_dim = 6)
+lyapunov_max = nolds.lyap_r(data = data)
 
 print("maximun lyapunov exponent of Henon is ")
 print(lyapunov_max)
