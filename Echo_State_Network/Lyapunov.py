7dde630fdf6d0baedfc98e2526d78a0d6f7ffaa7
RyoheiSheep <48007637+RyoheiSheep@users.noreply.github.com>
2020-02-07 00:57:16 +0900

    最大リアプノフ指数計算

--- Echo_State_Network/Lyapunov.py
+++ Echo_State_Network/Lyapunov.py
@@ -1,10 +1,12 @@
 import nolds
 import numpy as np
 
-data = np.loadtxt("Henon_reservoir_states_node100.txt").T[1,:500000]
-
-lyapunov_max = nolds.lyap_r(data = data, emb_dim = 10)
-
-np.savetxt("Henon_reservoir_maximum_lyapunov_emb10_node1.txt", lyapunov_max)
-print("maximun lyapunov exponent of Henon is ")
+data = np.loadtxt("Rossler_reservoir_states_node100_no1.txt")[:40000]
+lyapunov_max = np.zeros((5,1))
+for i in range(4):
+    
+    lyapunov_max[i] = np.array([i+1, nolds.lyap_r(data = data[:10000*(i+1)], emb_dim =5)])
+    
+np.savetxt("Rossler_reservoir_maximum_lyapunov_emb4_node1.txt", lyapunov_max)
+print("maximun lyapunov exponent of Rossler is ")
 print(lyapunov_max)
